f4aad53a6ef5fa1d8a8e3ade09eefb1bf5bf285f
psowers <sowerspa@gmail.com>
2016-05-25 23:14:57 -0500

    Some refactoring and formatting.

---
+++ convert2flat.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+Input file:
+    Core ID  Decade  Measurement(s)   [Site ID]
+    col 1-6   9-12      13-72         74-78(80)
+    source: ftp://ftp.ncdc.noaa.gov/pub/data/paleo/treering/treeinfo.txt
+    http://www.voortech.com/projectj2x/docs/userGuide.htm#Appendix 1.
+
+Output file:
+    Core ID   Year  Measurement  [Site ID]
+    col 1-6   9-12      13-18      20-24
+"""
+
+from treerings import *
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        FilesIn = [os.path.abspath(arg) for arg in sys.argv[1:] if os.path.isfile(arg)]
+        for arg in sys.argv[1:]:
+            if not os.path.abspath(arg) in FilesIn:
+                print 'Argument not vaild: "%s"' % arg
+    else:
+        FilesIn = [promptfile()]
+
+    for Fin in FilesIn:
+        if is_readable(Fin):
+            Fout_nm = unique_nm(os.path.splitext(Fin)[0] + '.txt')
+            raw2flat(Fin, Fout_nm)
